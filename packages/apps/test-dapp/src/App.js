693e8e3ec408ee0ca27d72051026e41de5772da7
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2019-08-02 23:29:56 +0300

    wallet address is passed to dapp

--- packages/apps/test-dapp/src/App.js
+++ packages/apps/test-dapp/src/App.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import './App.css'
 
+const WALLET_URL = 'http://localhost:3000'
+
 class App extends React.Component {
   async componentDidMount () {
     console.log('component did mount')
+    const walletWindow = window.opener
+    const walletOrigin = WALLET_URL
+    const message = 'Hello Wallet'
+    walletWindow.postMessage(message, walletOrigin)
+
+    window.addEventListener('message', this.receiveMessage.bind(this), false)
+  }
+
+  receiveMessage (event) {
+    // Do we trust the sender of this message?
+    if (event.origin !== WALLET_URL) return
+    console.log('got event from wallet: ', { event })
+    // event.source is window.opener
+    // event.data is 'hello there!'
   }
   
   render () {
