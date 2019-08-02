b29c061282b40551ece84f8ebbaed02e3350f67b
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2019-08-02 22:52:39 +0300

    private key generation added to test-wallet

--- packages/apps/test-wallet/src/App.js
+++ packages/apps/test-wallet/src/App.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Wallet } from 'ethers'
 import logo from './logo.svg'
 import './App.css'
 
+
 class App extends React.Component {
+  async componentDidMount () {
+    console.log('component did mount')
+    let pk = await window.localStorage.getItem('pkKey')
+    console.log({ pk })
+    if (!pk) {
+      console.log('Generating new private key..')
+      pk = Wallet.createRandom().privateKey
+      console.log({ pk })
+      window.localStorage.setItem('pkKey', pk)
+    } else {
+      console.log('found existing pk: ', pk)
+    }
+  }
+  
   render () {
     return (
         <div className='App'>
