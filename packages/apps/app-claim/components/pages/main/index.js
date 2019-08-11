ea9c7c2dee1ef3ba69aa266dffefda5bbe0a9e3e
spacehaz <spacehaz@gmail.com>
2019-08-11 13:02:22 +0300

    added fix for wallet changes

--- packages/apps/app-claim/components/pages/main/index.js
+++ packages/apps/app-claim/components/pages/main/index.js
@@ -91,7 +91,7 @@ class Main extends React.Component {
   }
 
   renderCurrentPage ({ context }) {
-    const { itemsToClaim, userLoading, errors, alreadyClaimed } = this.props
+    const { itemsToClaim, userLoading, errors, alreadyClaimed, step } = this.props
     // in context we can find:
     // active,
     // connectorName,
@@ -100,7 +100,6 @@ class Main extends React.Component {
     // networkId,
     // account,
     // error
-    const step = 3
     const {
       account,
       networkId
