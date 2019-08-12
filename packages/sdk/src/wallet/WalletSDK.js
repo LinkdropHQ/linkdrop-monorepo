b9a3f1573cd2097f4aac8b7ba23627d50341a720
Amir Jumaniyazov <amiromayer@gmail.com>
2019-08-12 22:42:52 +0500

    Added create future wallet

--- packages/sdk/src/wallet/WalletSDK.js
+++ packages/sdk/src/wallet/WalletSDK.js
@@ -1,4 +1,14 @@
 import UniversalLoginSDK from '@universal-login/sdk'
+import { LinkdropSDK } from '@linkdrop/sdk'
+import { BalanceObserver } from '@universal-login/sdk/dist/lib/core/observers/BalanceObserver'
+import { FutureWalletFactory } from '@universal-login/sdk/dist/lib/api/FutureWalletFactory'
+import {
+  calculateInitializeSignature,
+  ensureNotNull
+} from '@universal-login/commons'
+
+import { getString } from '../../../scripts/src/utils'
+const LINKDROP_FACTORY_ADDRESS = getString('FACTORY_ADDRESS')
 
 class WalletSDK {
   constructor (chain = 'rinkeby') {
@@ -7,13 +17,113 @@ class WalletSDK {
     }
 
     this.chain = chain
+    this.jsonRpcUrl = `https://${chain}.infura.io`
 
     this.sdk = new UniversalLoginSDK(
-      `https://${chain}.ul-relayer.linkdrop.io`,
-      `https://${chain}.infura.io`
+      'http://rinkeby.linkdrop.io:1104',
+      this.jsonRpcUrl
+    )
+  }
+
+  async claim ({
+    weiAmount,
+    tokenAddress,
+    tokenAmount,
+    expirationTime,
+    linkKey,
+    linkdropMasterAddress,
+    linkdropSignerSignature,
+    receiverAddress,
+    campaignId
+  }) {
+    //
+    const linkdropSDK = LinkdropSDK({
+      linkdropMasterAddress,
+      chain: this.chain,
+      jsonRpcUrl: this.jsonRpcUrl,
+      apiHost: `https://${this.chain}.linkdrop.io`,
+      factoryAddress: LINKDROP_FACTORY_ADDRESS
+    })
+
+    return linkdropSDK.claim({
+      weiAmount,
+      tokenAddress,
+      tokenAmount,
+      expirationTime,
+      linkKey,
+      linkdropSignerSignature,
+      receiverAddress,
+      campaignId
+    })
+  }
+
+  async _getFutureWalletFactory () {
+    ensureNotNull(
+      this.sdk.config,
+      Error,
+      'Relayer configuration not yet loaded'
     )
 
-    return this.sdk
+    const futureWalletConfig = {
+      supportedTokens: this.sdk.config.supportedTokens,
+      factoryAddress: this.sdk.config.factoryAddress,
+      contractWhiteList: this.sdk.config.contractWhiteList,
+      chainSpec: this.sdk.config.chainSpec
+    }
+    this.sdk.futureWalletFactory =
+      this.sdk.futureWalletFactory ||
+      new FutureWalletFactory(
+        futureWalletConfig,
+        this.sdk.provider,
+        this.sdk.blockchainService,
+        this.sdk.relayerApi
+      )
+  }
+
+  async createFutureWallet () {
+    await this.sdk.getRelayerConfig()
+    await this._getFutureWalletFactory()
+
+    const [
+      privateKey,
+      contractAddress,
+      publicKey
+    ] = await this.sdk.futureWalletFactory.blockchainService.createFutureWallet(
+      this.sdk.futureWalletFactory.config.factoryAddress
+    )
+
+    const waitForBalance = async () =>
+      new Promise(resolve => {
+        const onReadyToDeploy = (tokenAddress, contractAddress) =>
+          resolve({ tokenAddress, contractAddress })
+        const balanceObserver = new BalanceObserver(
+          this.sdk.futureWalletFactory.config.supportedTokens,
+          this.sdk.futureWalletFactory.provider
+        )
+        balanceObserver.startAndSubscribe(contractAddress, onReadyToDeploy)
+      })
+
+    const deploy = async (ensName, gasPrice) => {
+      const initData = await this.sdk.futureWalletFactory.setupInitData(
+        publicKey,
+        ensName,
+        gasPrice
+      )
+      const signature = await calculateInitializeSignature(initData, privateKey)
+      return this.sdk.futureWalletFactory.relayerApi.deploy(
+        publicKey,
+        ensName,
+        gasPrice,
+        signature
+      )
+    }
+
+    return {
+      privateKey,
+      contractAddress,
+      waitForBalance,
+      deploy
+    }
   }
 }
 
