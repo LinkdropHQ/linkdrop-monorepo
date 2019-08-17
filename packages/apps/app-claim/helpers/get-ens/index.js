1e6048490774944f6140530561bf8d464ff5eb75
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2019-08-17 15:15:02 +0300

    ens underscore fix

--- packages/apps/app-claim/helpers/get-ens/index.js
+++ packages/apps/app-claim/helpers/get-ens/index.js
@@ -1,1 +1,1 @@
-export default ({ email = '' }) => email.slice(0, email.indexOf('@')).replace('.', '_') + '.linkdrop.test'
+export default ({ email = '' }) => email.slice(0, email.indexOf('@')).replace('.', '-') + '.linkdrop.test'
