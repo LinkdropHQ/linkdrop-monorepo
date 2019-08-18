6fd09b8f47bee355394066d1409db630a9d81b04
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2019-08-18 19:20:14 +0200

    3 chars added

--- packages/apps/app-claim/helpers/get-ens/index.js
+++ packages/apps/app-claim/helpers/get-ens/index.js
@@ -1,4 +1,5 @@
 export default ({ email = '', chainId = '1' }) => {
   const domain = chainId === '1' ? 'eth2phone.eth' : 'linkdrop.test'
-  return email.slice(0, email.indexOf('@')).replace('.', '-') + `.${domain}`
+  const randomChars = Math.floor(Math.random() * (999 - 100 + 1) + 100)
+  return email.slice(0, email.indexOf('@')).replace('.', '-') + `-${randomChars}.${domain}`
 }
