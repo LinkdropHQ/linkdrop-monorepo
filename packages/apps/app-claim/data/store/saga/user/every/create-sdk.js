62fec4ce7ee300fa485ddc0b66ba66d1489d78be
spacehaz <spacehaz@gmail.com>
2019-08-24 19:17:23 +0200

    options

--- packages/apps/app-claim/data/store/saga/user/every/create-sdk.js
+++ packages/apps/app-claim/data/store/saga/user/every/create-sdk.js
@@ -1,13 +1,13 @@
 import { put } from 'redux-saga/effects'
 import { initializeWalletSdk } from 'data/sdk'
-import { infuraPk } from 'config'
+import config from 'app.config.js'
 import { defineNetworkName } from '@linkdrop/commons'
 
 const generator = function * ({ payload }) {
   try {
     const { chainId = '4' } = payload
     const networkName = defineNetworkName({ chainId })
-    const sdk = initializeWalletSdk({ chain: networkName, infuraPk })
+    const sdk = initializeWalletSdk({ chain: networkName, infuraPk: config.infuraPk })
     yield put({ type: 'USER.SET_SDK', payload: { sdk } })
   } catch (e) {
     console.error(e)
