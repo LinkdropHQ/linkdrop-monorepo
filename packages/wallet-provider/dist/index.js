498e9549be7ca3b1490606221012af69610494f0
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2019-09-09 03:54:19 +0300

    wallet provider lib dist files compiled

--- packages/wallet-provider/dist/index.js
+++ packages/wallet-provider/dist/index.js
@@ -15,6 +15,10 @@ var _classCallCheck2 = _interopRequireDefault(require("@babel/runtime/helpers/cl
 
 var _createClass2 = _interopRequireDefault(require("@babel/runtime/helpers/createClass"));
 
+var _connectToChild = _interopRequireDefault(require("penpal/lib/connectToChild"));
+
+var _styles = require("./styles");
+
 var ProviderEngine = require('web3-provider-engine');
 
 var RpcSubprovider = require('web3-provider-engine/subproviders/rpc');
@@ -29,8 +33,6 @@ var HookedWalletSubprovider = require('web3-provider-engine/subproviders/hooked-
 
 var SubscriptionsSubprovider = require('web3-provider-engine/subproviders/subscriptions.js');
 
-var ethers = require('ethers');
-
 var Provider =
 /*#__PURE__*/
 function () {
@@ -39,7 +41,7 @@ function () {
     this.ensName = opts.ensName;
     this.network = opts.network || 'mainnet';
     this.rpcUrl = opts.rpcUrl || "https://".concat(this.network, ".infura.io/v3/d4d1a2b933e048e28fb6fe1abe3e813a");
-    this.confirmUrl = opts.confirmUrl;
+    this.widgetUrl = opts.widgetUrl;
 
     if (!opts.ensName) {
       throw new Error('ENS name should be provided');
@@ -49,66 +51,93 @@ function () {
       throw new Error('network should be provided');
     }
 
+    this.widget = null;
     this.provider = this._initProvider();
   }
 
   (0, _createClass2["default"])(Provider, [{
-    key: "_getAddressFromEns",
-    value: function () {
-      var _getAddressFromEns2 = (0, _asyncToGenerator2["default"])(
-      /*#__PURE__*/
-      _regenerator["default"].mark(function _callee(ensName, network) {
-        var address, provider;
-        return _regenerator["default"].wrap(function _callee$(_context) {
-          while (1) {
-            switch (_context.prev = _context.next) {
-              case 0:
-                _context.prev = 0;
-                provider = ethers.getDefaultProvider(network);
-                _context.next = 4;
-                return provider.resolveName(ensName);
+    key: "_initWidget",
+    value: function _initWidget() {
+      var _this = this;
 
-              case 4:
-                address = _context.sent;
-                _context.next = 10;
-                break;
+      return new Promise(function (resolve, reject) {
+        var onload =
+        /*#__PURE__*/
+        function () {
+          var _ref = (0, _asyncToGenerator2["default"])(
+          /*#__PURE__*/
+          _regenerator["default"].mark(function _callee() {
+            var style, container, iframe, connection, communication;
+            return _regenerator["default"].wrap(function _callee$(_context) {
+              while (1) {
+                switch (_context.prev = _context.next) {
+                  case 0:
+                    style = document.createElement('style');
+                    style.innerHTML = _styles.styles;
+                    container = document.createElement('div');
+                    container.className = 'ld-widget-container';
+                    iframe = document.createElement('iframe');
+                    iframe.src = _this.widgetUrl || 'https://demo.wallet.linkdrop.io/#/widget';
+                    iframe.className = 'ld-widget-iframe';
+                    container.appendChild(iframe);
+                    document.body.appendChild(container);
+                    document.head.appendChild(style);
+                    connection = (0, _connectToChild["default"])({
+                      // The iframe to which a connection should be made
+                      iframe: iframe,
+                      // Methods the parent is exposing to the child
+                      methods: {
+                        showWidget: _this._showWidget.bind(_this),
+                        hideWidget: _this._hideWidget.bind(_this)
+                      }
+                    });
+                    _context.next = 13;
+                    return connection.promise;
 
-              case 7:
-                _context.prev = 7;
-                _context.t0 = _context["catch"](0);
-                throw new Error('Bad ENS name provided');
+                  case 13:
+                    communication = _context.sent;
+                    resolve({
+                      iframe: iframe,
+                      communication: communication
+                    });
 
-              case 10:
-                return _context.abrupt("return", address);
+                  case 15:
+                  case "end":
+                    return _context.stop();
+                }
+              }
+            }, _callee);
+          }));
 
-              case 11:
-              case "end":
-                return _context.stop();
-            }
-          }
-        }, _callee, null, [[0, 7]]);
-      }));
+          return function onload() {
+            return _ref.apply(this, arguments);
+          };
+        }();
 
-      function _getAddressFromEns(_x, _x2) {
-        return _getAddressFromEns2.apply(this, arguments);
-      }
-
-      return _getAddressFromEns;
-    }()
+        if (['loaded', 'interactive', 'complete'].indexOf(document.readyState) > -1) {
+          onload();
+        } else {
+          window.addEventListener('load', onload.bind(_this), false);
+        }
+      });
+    }
   }, {
-    key: "_getConfirmationUrlFromEns",
-    value: function _getConfirmationUrlFromEns(ensName) {
-      if (this.confirmUrl) return this.confirmUrl;
-      return 'https://demo.wallet.linkdrop.io/#/confirm';
+    key: "_showWidget",
+    value: function _showWidget() {
+      this.widget.iframe.style.display = 'block';
+    }
+  }, {
+    key: "_hideWidget",
+    value: function _hideWidget() {
+      this.widget.iframe.style.display = 'none';
     }
   }, {
     key: "_initProvider",
     value: function _initProvider() {
-      var _this = this;
+      var _this2 = this;
 
       var engine = new ProviderEngine();
       var address;
-      var confirmationUrl;
       engine.enable =
       /*#__PURE__*/
       (0, _asyncToGenerator2["default"])(
@@ -119,13 +148,14 @@ function () {
             switch (_context2.prev = _context2.next) {
               case 0:
                 _context2.next = 2;
-                return _this._getAddressFromEns(_this.ensName, _this.network);
+                return _this2._initWidget();
 
               case 2:
-                address = _context2.sent;
-                confirmationUrl = _this._getConfirmationUrlFromEns(_this.ensName);
+                _this2.widget = _context2.sent;
+                _context2.next = 5;
+                return _this2.widget.communication.connect();
 
-              case 4:
+              case 5:
               case "end":
                 return _context2.stop();
             }
@@ -133,32 +163,32 @@ function () {
         }, _callee2);
       }));
 
-      function _handleRequest2(_x3) {
+      function _handleRequest2(_x) {
         return _handleRequest.apply(this, arguments);
       }
 
       function _handleRequest() {
         _handleRequest = (0, _asyncToGenerator2["default"])(
         /*#__PURE__*/
-        _regenerator["default"].mark(function _callee5(payload) {
+        _regenerator["default"].mark(function _callee7(payload) {
           var result, message;
-          return _regenerator["default"].wrap(function _callee5$(_context5) {
+          return _regenerator["default"].wrap(function _callee7$(_context7) {
             while (1) {
-              switch (_context5.prev = _context5.next) {
+              switch (_context7.prev = _context7.next) {
                 case 0:
                   result = null;
-                  _context5.prev = 1;
-                  _context5.t0 = payload.method;
-                  _context5.next = _context5.t0 === 'eth_accounts' ? 5 : _context5.t0 === 'eth_coinbase' ? 7 : _context5.t0 === 'eth_chainId' ? 9 : _context5.t0 === 'net_version' ? 10 : _context5.t0 === 'eth_uninstallFilter' ? 11 : 14;
+                  _context7.prev = 1;
+                  _context7.t0 = payload.method;
+                  _context7.next = _context7.t0 === 'eth_accounts' ? 5 : _context7.t0 === 'eth_coinbase' ? 7 : _context7.t0 === 'eth_chainId' ? 9 : _context7.t0 === 'net_version' ? 10 : _context7.t0 === 'eth_uninstallFilter' ? 11 : 14;
                   break;
 
                 case 5:
                   result = [address];
-                  return _context5.abrupt("break", 16);
+                  return _context7.abrupt("break", 16);
 
                 case 7:
                   result = address;
-                  return _context5.abrupt("break", 16);
+                  return _context7.abrupt("break", 16);
 
                 case 9:
                   throw new Error('eth_chainId call not implemented');
@@ -171,23 +201,23 @@ function () {
                     return _;
                   });
                   result = true;
-                  return _context5.abrupt("break", 16);
+                  return _context7.abrupt("break", 16);
 
                 case 14:
                   message = "Card Web3 object does not support synchronous methods like ".concat(payload.method, " without a callback parameter.");
                   throw new Error(message);
 
                 case 16:
-                  _context5.next = 21;
+                  _context7.next = 21;
                   break;
 
                 case 18:
-                  _context5.prev = 18;
-                  _context5.t1 = _context5["catch"](1);
-                  throw _context5.t1;
+                  _context7.prev = 18;
+                  _context7.t1 = _context7["catch"](1);
+                  throw _context7.t1;
 
                 case 21:
-                  return _context5.abrupt("return", {
+                  return _context7.abrupt("return", {
                     id: payload.id,
                     jsonrpc: payload.jsonrpc,
                     result: result
@@ -195,10 +225,10 @@ function () {
 
                 case 22:
                 case "end":
-                  return _context5.stop();
+                  return _context7.stop();
               }
             }
-          }, _callee5, null, [[1, 18]]);
+          }, _callee7, null, [[1, 18]]);
         }));
         return _handleRequest.apply(this, arguments);
       }
@@ -206,7 +236,7 @@ function () {
       engine.send =
       /*#__PURE__*/
       function () {
-        var _ref2 = (0, _asyncToGenerator2["default"])(
+        var _ref3 = (0, _asyncToGenerator2["default"])(
         /*#__PURE__*/
         _regenerator["default"].mark(function _callee3(payload, callback) {
           var res;
@@ -259,8 +289,8 @@ function () {
           }, _callee3);
         }));
 
-        return function (_x4, _x5) {
-          return _ref2.apply(this, arguments);
+        return function (_x2, _x3) {
+          return _ref3.apply(this, arguments);
         };
       }();
 
@@ -276,51 +306,98 @@ function () {
 
       var cacheSubprovider = new CacheSubprovider(); // hack to deal with multiple received messages via PostMessage
 
-      var cache = {};
       var walletSubprovider = new HookedWalletSubprovider({
-        getAccounts: function getAccounts(cb) {
-          var result = [address];
-          var error = null;
-          cb(error, result);
-        },
-        processTransaction: function processTransaction(txParams, cb) {
-          var receiveMessage = function receiveMessage(event) {
-            // Do we trust the sender of this message?
-            // if (event.origin !== confirmationUrl) return
-            if (event.origin !== confirmationUrl.substring(event.origin.length, -1)) return;
+        getAccounts: function () {
+          var _getAccounts = (0, _asyncToGenerator2["default"])(
+          /*#__PURE__*/
+          _regenerator["default"].mark(function _callee4(cb) {
+            var result, error;
+            return _regenerator["default"].wrap(function _callee4$(_context4) {
+              while (1) {
+                switch (_context4.prev = _context4.next) {
+                  case 0:
+                    _context4.prev = 0;
+                    _context4.next = 3;
+                    return _this2.widget.communication.getAccounts();
 
-            if (event.data.action === 'PASS_TRANSACTION_RESULT') {
-              var _event$data$payload = event.data.payload,
-                  success = _event$data$payload.success,
-                  txHash = _event$data$payload.txHash;
+                  case 3:
+                    result = _context4.sent;
+                    _context4.next = 9;
+                    break;
 
-              if (cache[txHash]) {
-                return null;
+                  case 6:
+                    _context4.prev = 6;
+                    _context4.t0 = _context4["catch"](0);
+                    error = _context4.t0;
+
+                  case 9:
+                    cb(error, result);
+
+                  case 10:
+                  case "end":
+                    return _context4.stop();
+                }
               }
+            }, _callee4, null, [[0, 6]]);
+          }));
 
-              cache[txHash] = true;
+          function getAccounts(_x4) {
+            return _getAccounts.apply(this, arguments);
+          }
 
-              if (success) {
-                cb(null, txHash);
-              } else {
-                var error = 'Transaction was rejected by user';
-                cb(error);
+          return getAccounts;
+        }(),
+        processTransaction: function () {
+          var _processTransaction = (0, _asyncToGenerator2["default"])(
+          /*#__PURE__*/
+          _regenerator["default"].mark(function _callee5(txParams, cb) {
+            var result, error, _ref4, txHash, success, errors;
+
+            return _regenerator["default"].wrap(function _callee5$(_context5) {
+              while (1) {
+                switch (_context5.prev = _context5.next) {
+                  case 0:
+                    _context5.prev = 0;
+                    _context5.next = 3;
+                    return _this2.widget.communication.sendTransaction(txParams);
+
+                  case 3:
+                    _ref4 = _context5.sent;
+                    txHash = _ref4.txHash;
+                    success = _ref4.success;
+                    errors = _ref4.errors;
+
+                    if (success) {
+                      result = txHash;
+                    } else {
+                      error = errors[0] || 'Error while sending transaction';
+                    }
+
+                    _context5.next = 13;
+                    break;
+
+                  case 10:
+                    _context5.prev = 10;
+                    _context5.t0 = _context5["catch"](0);
+                    error = _context5.t0;
+
+                  case 13:
+                    cb(error, result);
+
+                  case 14:
+                  case "end":
+                    return _context5.stop();
+                }
               }
-            }
-          };
+            }, _callee5, null, [[0, 10]]);
+          }));
 
-          window.addEventListener('message', receiveMessage, false);
-          var newWindow = window.open(confirmationUrl, '_blank');
-          setTimeout(function () {
-            var data = {
-              action: 'SEND_TRANSACTION',
-              payload: {
-                txParams: txParams
-              }
-            };
-            newWindow.postMessage(data, confirmationUrl);
-          }, 1000);
-        }
+          function processTransaction(_x5, _x6) {
+            return _processTransaction.apply(this, arguments);
+          }
+
+          return processTransaction;
+        }()
       });
       /* ADD MIDDELWARE (PRESERVE ORDER) */
 
@@ -338,38 +415,38 @@ function () {
         handleRequest: function () {
           var _handleRequest3 = (0, _asyncToGenerator2["default"])(
           /*#__PURE__*/
-          _regenerator["default"].mark(function _callee4(payload, next, end) {
-            var _ref3, result;
+          _regenerator["default"].mark(function _callee6(payload, next, end) {
+            var _ref5, result;
 
-            return _regenerator["default"].wrap(function _callee4$(_context4) {
+            return _regenerator["default"].wrap(function _callee6$(_context6) {
               while (1) {
-                switch (_context4.prev = _context4.next) {
+                switch (_context6.prev = _context6.next) {
                   case 0:
-                    _context4.prev = 0;
-                    _context4.next = 3;
+                    _context6.prev = 0;
+                    _context6.next = 3;
                     return _handleRequest2(payload);
 
                   case 3:
-                    _ref3 = _context4.sent;
-                    result = _ref3.result;
+                    _ref5 = _context6.sent;
+                    result = _ref5.result;
                     end(null, result);
-                    _context4.next = 11;
+                    _context6.next = 11;
                     break;
 
                   case 8:
-                    _context4.prev = 8;
-                    _context4.t0 = _context4["catch"](0);
-                    end(_context4.t0);
+                    _context6.prev = 8;
+                    _context6.t0 = _context6["catch"](0);
+                    end(_context6.t0);
 
                   case 11:
                   case "end":
-                    return _context4.stop();
+                    return _context6.stop();
                 }
               }
-            }, _callee4, null, [[0, 8]]);
+            }, _callee6, null, [[0, 8]]);
           }));
 
-          function handleRequest(_x6, _x7, _x8) {
+          function handleRequest(_x7, _x8, _x9) {
             return _handleRequest3.apply(this, arguments);
           }
 
