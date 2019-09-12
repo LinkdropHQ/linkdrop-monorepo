d392a5a5fd68decc77d3acb19a409b9b7ba0e9bb
Amir Jumaniyazov <amiromayer@gmail.com>
2019-09-12 15:45:39 +0500

    Added routes and controller for ens

--- packages/safe-relayer/src/controllers/safes.js
+++ packages/safe-relayer/src/controllers/safes.js
@@ -5,13 +5,40 @@ import assert from 'assert-js'
 import wrapAsync from '../utils/asyncWrapper'
 import safeCreationService from '../services/safeCreationService'
 
-export const createSafe = wrapAsync(async (req, res, next) => {
+export const create = wrapAsync(async (req, res, next) => {
   try {
     const { owner } = req.body
     assert.string(owner, 'Owner is required')
 
     const { success, txHash, safe, errors } = await safeCreationService.create({
       owner
+    })
+
+    res.json({
+      success,
+      txHash,
+      safe,
+      errors
+    })
+  } catch (err) {
+    next(err)
+  }
+})
+
+export const createWithENS = wrapAsync(async (req, res, next) => {
+  try {
+    const { owner, name } = req.body
+    assert.string(owner, 'Owner is required')
+    assert.string(name, 'Name is required')
+
+    const {
+      success,
+      txHash,
+      safe,
+      errors
+    } = await safeCreationService.createWithENS({
+      owner,
+      name
     })
 
     res.json({
--- packages/safe-relayer/src/routes/safes.js
+++ packages/safe-relayer/src/routes/safes.js
@@ -4,10 +4,17 @@ import * as safesController from '../controllers/safes'
 const router = express.Router()
 
 /**
- * @route POST api/v1/safes
+ * @route POST api/v1/safes/create
  * @desc Create new safe
  * @access Public
  */
-router.post('/', safesController.createSafe)
+router.post('/create', safesController.create)
+
+/**
+ * @route POST api/v1/safes/createWithENS
+ * @desc Create new safe and initialize with ens name
+ * @access Public
+ */
+router.post('/createWithENS', safesController.createWithENS)
 
 export default router
