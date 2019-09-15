f364b0e1bc8ebe8d2e3c10a8930575c05b62461e
spacehaz <spacehaz@gmail.com>
2019-09-15 12:07:56 +0300

    addded fix

---
+++ packages/apps/app-claim/data/store/reducers/widget/index.js
@@ -0,0 +1,18 @@
+import reducers from './reducers'
+
+const initialState = {
+  page: undefined
+}
+
+export default (state = initialState, action = {}) => {
+  const newState = { ...state }
+  const { type } = action
+  const actionMethod = ACTIONS[type]
+  if (!actionMethod) return newState
+
+  return actionMethod(newState, action)
+}
+
+const ACTIONS = {
+  'USER.SET_PAGE': reducers.setPage
+}
---
+++ packages/apps/app-claim/data/store/reducers/widget/reducers/index.js
@@ -0,0 +1,5 @@
+import setPage from './set-page'
+
+export default {
+  setPage
+}
