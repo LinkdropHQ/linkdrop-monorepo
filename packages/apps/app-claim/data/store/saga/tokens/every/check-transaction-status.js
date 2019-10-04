5f642d137e0e1762843d686dd43267bf20f92329
spacehaz <spacehaz@gmail.com>
2019-10-04 11:54:59 +0300

    added claim page fix

--- packages/apps/app-claim/data/store/saga/tokens/every/check-transaction-status.js
+++ packages/apps/app-claim/data/store/saga/tokens/every/check-transaction-status.js
@@ -1,12 +1,11 @@
 import { put } from 'redux-saga/effects'
 import { ethers } from 'ethers'
-import { defineNetworkName } from '@linkdrop/commons'
+import { jsonRpcUrl } from 'app.config.js'
 
 const generator = function * ({ payload }) {
   try {
-    const { transactionId, chainId } = payload
-    const networkName = defineNetworkName({ chainId })
-    const provider = yield ethers.getDefaultProvider(networkName)
+    const { transactionId } = payload
+    const provider = yield new ethers.providers.JsonRpcProvider(jsonRpcUrl)
     const receipt = yield provider.getTransactionReceipt(transactionId)
     if (receipt && receipt.confirmations != null && receipt.confirmations > 0) {
       yield put({ type: 'TOKENS.SET_TRANSACTION_STATUS', payload: { transactionStatus: 'claimed' } })
