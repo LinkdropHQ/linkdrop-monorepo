14090b1758d3c7ad11cf91e0b0ec83bc21bd3056
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2019-10-08 03:13:36 +0300

    provider selectedAddress hack for Nifty Wallet

--- packages/apps/app-dashboard/components/pages/page/metamask-injector/index.js
+++ packages/apps/app-dashboard/components/pages/page/metamask-injector/index.js
@@ -1,3 +1,4 @@
+/* global web3 */
 import React from 'react'
 import { actions, translate } from 'decorators'
 import { Button } from 'components/common'
@@ -20,7 +21,17 @@ class MetamaskInjector extends React.Component {
     })
   }
 
-  applyProvider (provider) {
+  async applyProvider (provider) {
+
+    // temp hack for Nifty wallet 
+    if (!provider.selectedAddress && web3 && web3.eth && web3.eth.accounts && web3.eth.accounts[0]) {
+      provider.selectedAddress = web3.eth.accounts[0]
+      provider.networkVersion = web3.version.network
+    }
+
+    // TODO: this should be changed as only Metamask suppports `provider.selectedAddress`
+    // better to use web3.eth.getAccounts() or web.eth.accounts[0]
+    // another TODO: don't use web3 as global variable!
     if (provider.selectedAddress) {
       this.actions().user.checkCurrentProvider()
     }
