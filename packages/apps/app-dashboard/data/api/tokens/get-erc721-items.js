3955ed04caa034053405ae7dae5648e696b3af4e
spacehaz <spacehaz@gmail.com>
2019-10-21 12:59:10 +0300

    added fix

--- packages/apps/app-dashboard/data/api/tokens/get-erc721-items.js
+++ packages/apps/app-dashboard/data/api/tokens/get-erc721-items.js
@@ -5,7 +5,8 @@ export default ({ address, networkName, orderBy = 'current_price', direction = '
   const getParams = prepareGetParams({
     owner: address,
     order_by: orderBy,
-    order_direction: direction
+    order_direction: direction,
+    limit: 300
   })
   const domainName = networkName === 'rinkeby' ? 'rinkeby-api.opensea.io' : 'api.opensea.io'
   return fetch(`https://${domainName}/api/v1/assets/${getParams}`)
