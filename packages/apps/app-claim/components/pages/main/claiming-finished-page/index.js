a07ec26b6f4bcff5906ecb01ca9eebe994c1dc27
spacehaz <spacehaz@gmail.com>
2019-10-23 17:49:22 +0300

    added fix for dapp link on last page

--- packages/apps/app-claim/components/pages/main/claiming-finished-page/index.js
+++ packages/apps/app-claim/components/pages/main/claiming-finished-page/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { Alert, Icons } from '@linkdrop/ui-kit'
+import { Alert, Icons, Button } from '@linkdrop/ui-kit'
 import { translate, actions } from 'decorators'
 import styles from './styles.module'
 import commonStyles from '../styles.module'
+import { getDappData } from 'helpers'
 import { getHashVariables, defineEtherscanUrl } from '@linkdrop/commons'
 import classNames from 'classnames'
 
@@ -10,7 +11,7 @@ import classNames from 'classnames'
 @translate('pages.main')
 class ClaimingFinishedPage extends React.Component {
   render () {
-    const { chainId } = getHashVariables()
+    const { chainId, dappId } = getHashVariables()
     const { transactionId, amount, symbol } = this.props
     return <div className={commonStyles.container}>
       <Alert icon={<Icons.Check />} className={styles.alert} />
@@ -25,7 +26,22 @@ class ClaimingFinishedPage extends React.Component {
           })
         }}
       />
+      {this.renderDappButton({ dappId, transactionId })}
     </div>
+  }
+
+  renderDappButton ({ dappId, transactionId }) {
+    const dappData = getDappData({ dappId })
+    if (!dappData) { return null }
+    return <Button
+      className={classNames(styles.button, {
+        [styles.disableTranslateX]: !transactionId
+      })}
+      target='_blank'
+      href={dappData.link}
+    >
+      {this.t('buttons.goTo', { dapp: dappData.name })}
+    </Button>
   }
 }
 
