05d9fa1fda8d8ad87c2b44124e515d725ddad6c7
spacehaz <spacehaz@gmail.com>
2019-11-27 22:43:45 +0300

    added fix for superrare dapp fortmatic

--- packages/apps/app-claim/components/application/router-provider/index.js
+++ packages/apps/app-claim/components/application/router-provider/index.js
@@ -2,17 +2,14 @@ import React, { useEffect } from 'react'
 import { useWeb3Context } from 'web3-react'
 import { Router } from 'react-router'
 import { ConnectedRouter } from 'connected-react-router'
-import { defineConnectors } from 'helpers'
 import { history } from 'data/store'
 import { Loading } from '@linkdrop/ui-kit'
 import AppRouter from '../router'
-import { getHashVariables } from '@linkdrop/commons'
+
 export default function RouterProvider () {
-  const { w } = getHashVariables()
-  const connectors = defineConnectors({ defaultWallet: w })
   const context = useWeb3Context()
   useEffect(() => {
-    context.setFirstValidConnector(connectors)
+    context.setFirstValidConnector(['MetaMask', 'Network'])
   }, [])
 
   if (!context.active && !context.error) {
