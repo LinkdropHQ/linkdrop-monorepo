25ea85dc4ec8092122883b210053730a23c3d887
spacehaz <spacehaz@gmail.com>
2019-11-27 15:30:06 +0300

    added fix for wallets

--- packages/apps/app-claim/components/application/router-provider/index.js
+++ packages/apps/app-claim/components/application/router-provider/index.js
@@ -2,14 +2,17 @@ import React, { useEffect } from 'react'
 import { useWeb3Context } from 'web3-react'
 import { Router } from 'react-router'
 import { ConnectedRouter } from 'connected-react-router'
+import { defineConnectors } from 'helpers'
 import { history } from 'data/store'
 import { Loading } from '@linkdrop/ui-kit'
 import AppRouter from '../router'
-
+import { getHashVariables } from '@linkdrop/commons'
 export default function RouterProvider () {
+  const { w } = getHashVariables()
+  const connectors = defineConnectors({ defaultWallet: w })
   const context = useWeb3Context()
   useEffect(() => {
-    context.setFirstValidConnector(['MetaMask', 'Network'])
+    context.setFirstValidConnector(connectors)
   }, [])
 
   if (!context.active && !context.error) {
