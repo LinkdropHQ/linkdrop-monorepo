b184db31e34ade7e905e1239d3a3247738decffd
spacehaz <spacehaz@gmail.com>
2019-12-06 11:49:18 +0300

    added fix for prod version check

--- packages/apps/moonpay-buy/src/components/pages/link-generate/finished/mobile-claim/index.js
+++ packages/apps/moonpay-buy/src/components/pages/link-generate/finished/mobile-claim/index.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import styles from './styles.module'
+import { getHashVariables } from '@linkdrop/commons'
 
 class MobileClaim extends React.Component {
 	render () {
-		return <div>mobile claim</div>
+			const { link } = getHashVariables()
+			const iframeSrc = decodeURIComponent(link)
+			return <div className={styles.claimContainer}>
+			<iframe
+	      frameBorder='0'
+	      height='100%'
+	      src={iframeSrc}
+	      width='100%'
+	    >
+	      <p>Your browser does not support iframes.</p>
+	    </iframe>
+	  </div>
 	}
 }
-
 export default MobileClaim
