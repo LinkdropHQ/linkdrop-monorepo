292bc1e37504ab7d2f1e7cd591d513d130c561af
Amir Jumaniyazov <amiromayer@gmail.com>
2019-12-06 19:27:57 +0500

    Add morgan

--- packages/server/server.js
+++ packages/server/server.js
@@ -5,11 +5,23 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const buildRouter = require('./src/routes')
+const morgan = require('morgan')
 
 // Apply middlewares
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(cors())
+
+morgan.token('body', function (req, res) {
+  return JSON.stringify(req.body, null, 2)
+})
+
+app.use(
+  morgan(
+    ':method :url :status :res[content-length] - :response-time ms \n:body',
+    { stream: logger.stream }
+  )
+)
 
 // connect to database
 connectDB()
--- packages/server/src/controllers/utilsController.js
+++ packages/server/src/controllers/utilsController.js
@@ -2,7 +2,6 @@ import { fetchCoinbaseDeepLink } from '../services/CoinbaseLinkService'
 
 export const getCoinbaseDeepLink = async (req, res) => {
   // claim transaction
-  console.log(req.body)
   const link = await fetchCoinbaseDeepLink({ url: req.body.url })
 
   // return tx hash in successful response
