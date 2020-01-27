6e76de3a40c1e107a095cbf6be55f879fcfbc375
Amir <Amir@amirs-MacBook-Pro.local>
2020-01-28 00:19:29 +0500

    Modify scripts

--- packages/scripts/src/generate_links.js
+++ packages/scripts/src/generate_links.js
@@ -5,6 +5,7 @@ import LinkdropSDK from '@linkdrop/sdk'
 import ora from 'ora'
 import { terminal as term } from 'terminal-kit'
 import { ethers } from 'ethers'
+import { AddressZero } from 'ethers/constants'
 import path from 'path'
 import fastcsv from 'fast-csv'
 import fs from 'fs'
@@ -24,7 +25,8 @@ const {
   FEE_AMOUNT,
   CALLBACK_DATA,
   TOKENS_AMOUNT,
-  TOKEN_ADDRESS
+  TOKEN_ADDRESS,
+  LINKS_NUMBER
 } = config
 
 ethers.errors.setLogLevel('error')
@@ -46,7 +48,10 @@ const linkdropSDK = new LinkdropSDK({
 })
 
 export const generate = async () => {
-  let spinner, tx
+  let spinner, tx, tokenIds
+
+  let linksNumber = LINKS_NUMBER
+
   try {
     spinner = ora({
       text: term.bold.green.str('Generating links'),
@@ -59,67 +64,79 @@ export const generate = async () => {
     // check that proxy address is deployed
     await deployProxyIfNeeded(spinner)
 
-    const nftContract = await new ethers.Contract(
-      NFT_ADDRESS,
-      NFTMock.abi,
-      sender
-    )
-    const nftSymbol = await nftContract.symbol()
-
-    // If owner of tokenId is not proxy contract -> send it to proxy
-    const tokenIds = JSON.parse(TOKEN_IDS)
-
-    // Approve tokens
-    const isApprovedForAll = await nftContract.isApprovedForAll(
-      sender.address,
-      proxyAddress
-    )
-    if (!isApprovedForAll) {
-      spinner.info(
-        term.bold.str(`Approving all ${nftSymbol} to ^g${proxyAddress}`)
-      )
-
-      tx = await nftContract.setApprovalForAll(proxyAddress, true, {
-        gasLimit: 500000
-      })
-      term.bold(`Tx Hash: ^g${tx.hash}\n`)
-    }
-
-    // Send tokens to proxy
-    if (tokensAmount.gt(0) && TOKEN_ADDRESS !== ethers.constants.AddressZero) {
-      const cost = tokensAmount.mul(tokenIds.length)
-
-      const tokenContract = await new ethers.Contract(
-        TOKEN_ADDRESS,
-        TokenMock.abi,
+    if (NFT_ADDRESS && NFT_ADDRESS !== '') {
+      const nftContract = await new ethers.Contract(
+        NFT_ADDRESS,
+        NFTMock.abi,
         sender
       )
-      const tokenSymbol = await tokenContract.symbol()
-      const tokenDecimals = await tokenContract.decimals()
+      const nftSymbol = await nftContract.symbol()
+
+      // If owner of tokenId is not proxy contract -> send it to proxy
+      tokenIds = JSON.parse(TOKEN_IDS)
+
+      linksNumber = tokenIds.length
 
       // Approve tokens
-      const proxyAllowance = await tokenContract.allowance(
+      const isApprovedForAll = await nftContract.isApprovedForAll(
         sender.address,
         proxyAddress
       )
-      if (proxyAllowance.lt(cost)) {
+      if (!isApprovedForAll) {
         spinner.info(
-          term.bold.str(
-            `Approving ${cost /
-              Math.pow(10, tokenDecimals)} ${tokenSymbol} to ^g${proxyAddress}`
-          )
+          term.bold.str(`Approving all ${nftSymbol} to ^g${proxyAddress}`)
         )
 
-        tx = await tokenContract.approve(proxyAddress, cost, {
-          gasLimit: 600000
+        tx = await nftContract.setApprovalForAll(proxyAddress, true, {
+          gasLimit: 500000
         })
         term.bold(`Tx Hash: ^g${tx.hash}\n`)
       }
     }
 
+    if (TOKEN_ADDRESS && TOKEN_ADDRESS !== '') {
+      // Send tokens to proxy
+      if (
+        tokensAmount.gt(0) &&
+        TOKEN_ADDRESS !== ethers.constants.AddressZero
+      ) {
+        const cost = tokensAmount.mul(linksNumber)
+
+        const tokenContract = await new ethers.Contract(
+          TOKEN_ADDRESS,
+          TokenMock.abi,
+          sender
+        )
+        const tokenSymbol = await tokenContract.symbol()
+        const tokenDecimals = await tokenContract.decimals()
+
+        // Approve tokens
+        const proxyAllowance = await tokenContract.allowance(
+          sender.address,
+          proxyAddress
+        )
+        if (proxyAllowance.lt(cost)) {
+          spinner.info(
+            term.bold.str(
+              `Approving ${cost /
+                Math.pow(
+                  10,
+                  tokenDecimals
+                )} ${tokenSymbol} to ^g${proxyAddress}`
+            )
+          )
+
+          tx = await tokenContract.approve(proxyAddress, cost, {
+            gasLimit: 600000
+          })
+          term.bold(`Tx Hash: ^g${tx.hash}\n`)
+        }
+      }
+    }
+
     if (nativeTokensAmount.gt(0)) {
       // Transfer ethers
-      const cost = nativeTokensAmount.mul(tokenIds.length)
+      const cost = nativeTokensAmount.mul(linksNumber)
       let amountToSend
 
       const tokenSymbol = 'ETH'
@@ -146,7 +163,7 @@ export const generate = async () => {
       }
     }
 
-    const FEE_COSTS = feeAmount.mul(tokenIds.length)
+    const FEE_COSTS = feeAmount.mul(linksNumber)
     // Transfer fee coverage
     spinner.info(term.bold.str(`Sending fee costs to ^g${proxyAddress}`))
 
@@ -161,7 +178,7 @@ export const generate = async () => {
     // Generate links
     const links = []
 
-    for (let i = 0; i < tokenIds.length; i++) {
+    for (let i = 0; i < linksNumber; i++) {
       const {
         url,
         linkId,
@@ -169,11 +186,11 @@ export const generate = async () => {
         signerSignature
       } = await linkdropSDK.generateLink({
         signingKeyOrWallet: sender.privateKey,
-        nativeTokensAmount: NATIVE_TOKENS_AMOUNT,
-        nft: NFT_ADDRESS,
-        tokenId: tokenIds[i],
-        token: TOKEN_ADDRESS,
-        tokensAmount: TOKENS_AMOUNT,
+        nativeTokensAmount: NATIVE_TOKENS_AMOUNT || 0,
+        nft: NFT_ADDRESS || AddressZero,
+        tokenId: tokenIds ? tokenIds[i] : 0,
+        token: TOKEN_ADDRESS || AddressZero,
+        tokensAmount: TOKENS_AMOUNT || 0,
         campaignId: CAMPAIGN_ID,
         feeAmount: FEE_AMOUNT,
         data: CALLBACK_DATA
