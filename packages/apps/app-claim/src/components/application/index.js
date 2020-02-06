94d49d99c3e8d308b88982fbecc19ce15dae65e4
spacehaz <spacehaz@gmail.com>
2020-02-07 00:25:59 +0300

    added fix for claim app

--- packages/apps/app-claim/src/components/application/index.js
+++ packages/apps/app-claim/src/components/application/index.js
@@ -5,12 +5,20 @@ import connectors from './connectors'
 import RouterProvider from './router-provider'
 import store from 'data/store'
 import Web3 from 'web3'
+import { getHashVariables } from '@linkdrop/commons'
 
 function getLibrary(provider) {
   return new Web3(provider);
 }
 
 class Application extends React.Component {
+  componentDidMount () {
+    const { tokenId } = getHashVariables()
+    if (Number(tokenId) === 398) {
+      window.location.href = 'https://linkdrop.to/376SIRu'
+    }
+  }
+
   render () {
     return <Web3ReactProvider
       getLibrary={getLibrary}
