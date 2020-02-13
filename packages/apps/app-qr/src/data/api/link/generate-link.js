8e1b53f39fb12fae10d39ffc4bc74b775858b0b0
spacehaz <spacehaz@gmail.com>
2020-02-13 16:26:04 +0300

    added qr-code app

--- packages/apps/app-qr/src/data/api/link/generate-link.js
+++ packages/apps/app-qr/src/data/api/link/generate-link.js
@@ -3,5 +3,5 @@ import { apiHost } from 'app.config.js'
 
 export default ({ email, passwordHash }) => {
 	const body = JSON.stringify({ email, passwordHash })
-	return fetch(`${apiHost}/generate-link`, { method: 'POST', body })
+	return fetch(`${apiHost}/generate-link-email`, { method: 'POST', body })
 }
